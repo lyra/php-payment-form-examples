89864de712a34f2254854165de52133751c06404
moabi <moabi31@gmail.com>
2016-07-01 15:24:38 +0200

    return ex

---
+++ example/form-return.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * You can set the return URL in your back office to this file
+ * this will help you to get all the informations
+ */
+?>
+<h1>Form Return</h1>
+<?php
+include '../lib/payzenFormIpn.php';
+
+
