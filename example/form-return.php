5c106de74e6961827cd1d2769a7f478586cee852
moabi <moabi31@gmail.com>
2016-07-18 10:11:57 +0200

    bootstrap exemple

--- example/form-return.php
+++ example/form-return.php
@@ -5,7 +5,7 @@
  * By default, payzenIpn will write logs to a file
  *
  * to handle the Return URL of the shop
- * you can use the_payment_response() to get an html ouptput and display it to the the customer
+ * you can use the_payment_response() in payzenPaymentOutcome class to get an html ouptput and display it to the the customer
  *
  */
 include '../lib/payzenIpn.php';
