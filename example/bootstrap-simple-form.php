878e1a47bc053853bbee74982848f619f998fbbb
moabi <moabi31@gmail.com>
2016-07-19 08:34:04 +0200

    rm js in bootstrap ex

--- example/bootstrap-simple-form.php
+++ example/bootstrap-simple-form.php
@@ -79,7 +79,6 @@ $form .= '</form>';
     <meta name="description" content="">
     <meta name="author" content="">
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
     <title>Simple form</title>
 
 </head>
